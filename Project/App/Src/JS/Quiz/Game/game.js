5c0d88c7ec4f2583ff62c5113d39c22f3cfc3547
Kevinisky27 <keare27@gmail.com>
2022-05-16 00:55:17 -0600

    sección de preguntas 70%

---
+++ Project/App/Src/JS/Quiz/Game/game.js
@@ -0,0 +1,110 @@
+const question = document.querySelector('#question');
+const choices = Array.from(document.querySelectorAll('.choice-text'));
+const progressText = document.querySelector('#progressText');
+const scroreText = document.querySelector('#score');
+const progressBarFull = document.querySelector('#progressBarFull');
+
+let currentQuestion = {}
+let acceptingAnswers = true
+let score = 0
+let questionCounter = 0
+let availableQuestions = []
+
+let questions = [
+  {
+    question: '¿Cuánto es 2 + 2?', 
+    choice1: '2',
+    choice2: '4',
+    choice3: '21',
+    choice4: '17',
+    answer: 2
+  }, 
+  {
+    question: '¿Cuál no es un lenguaje de programación?', 
+    choice1: 'Java',
+    choice2: 'JavaScript',
+    choice3: 'HTML',
+    choice4: 'Ruby',
+    answer: 3
+  }, 
+  {
+    question: '¿Cómo se sabe que es un operador lógico en programación?', 
+    choice1: 'true / false',
+    choice2: 'if / else if',
+    choice3: 'true / if',
+    choice4: 'false / for',
+    answer: 1
+  }, 
+  {
+    question: '¿Qué significa py?', 
+    choice1: 'significa play',
+    choice2: 'es la abreviatura del formato de Python',
+    choice3: 'significa Paypal',
+    choice4: 'player',
+    answer: 2
+  }
+]
+
+const SCORE_POINTS = 100
+const MAX_QUESTIONS = 4
+
+starGame = () => {
+  questionCounter = 0; 
+  score = 0
+  availableQuestions = [...questions]
+  getNewQuestion ()
+}
+
+getNewQuestion = () => {
+  if (availableQuestions.length === 0 || questionCounter > MAX_QUESTIONS) {
+    localStorage.setItem('mostRecentScore', score)
+
+    return window.location.assign('/end.html')
+  }
+
+  questionCounter++
+  progressText.innerText = `Pregunta ${questionCounter} de ${MAX_QUESTIONS}`
+  progressBarFull.style.width = `${(questionCounter/MAX_QUESTIONS)* 100}%`
+  
+  const questionsIndex  = Math.floor(Math.random() * availableQuestions.length)
+  currentQuestion = availableQuestions[questionsIndex]
+  question.innerText = currentQuestion.question
+
+  choices.forEach(choice => {
+    const number = choice.dataset['number']
+    choice.innerText = currentQuestion['choice' + number]
+  })
+
+  availableQuestions.splice(questionsIndex, 1)
+  acceptingAnswers = true
+}
+
+choices.forEach(choice => {
+  choice.addEventListener('click', e => {
+    if (!acceptingAnswers) return
+
+    acceptingAnswers = false
+    const selectedChoice = e.target
+    const selectedAnswer = selectedChoice.dataset['number']
+
+    let classToApply = selectedAnswer == currentQuestion.answer ? 'correct' : 'incorrect'
+
+    if (classToApply === 'correct'){
+      incrementScore(SCORE_POINTS)
+    }
+
+    selectedChoice.parentElement.classList.add(classToApply)
+
+    setTimeout(() => {
+      selectedChoice.parentElement.classList.remove(classToApply)
+      getNewQuestion()
+    }, 1000)
+  } )
+})
+
+incrementScore = num => {
+  score += num
+  scroreText.innerText = score
+}
+
+starGame()
