e406251ee92ed8e0acb2ed4f05912c1075554701
Kevinisky27 <keare27@gmail.com>
2022-06-17 22:56:02 -0600

    Cambios a la plataforma

--- Project/App/Src/JS/Quiz/Game/end.js
+++ Project/App/Src/JS/Quiz/Game/end.js
@@ -1,7 +1,7 @@
 const username = document.querySelector('#username')
 const saveScoreBtn = document.querySelector('#saveScoreBtn')
 const finalScore = document.querySelector('#finalScore')
-const mostRecentScore = document.querySelector('#mostRecentScore')
+const mostRecentScore = localStorage.getItem('mostRecentScore')
 
 const highScore = JSON.parse(localStorage.getItem('highScore')) || []
 
@@ -27,9 +27,5 @@ saveHighScore = e => {
 
   highScore.splice(5)
   localStorage.setItem('highScores', JSON.stringify(highScores))
-  window.location.assign('/')
-}
-
-function repetirQuiz() {
-  location.href = "../../../Views/Quiz/Game/game_programacion.html";
+  window.location.assign('')
 }
